a7d989eff115f6ccf224037ff2369680516b2743
andreask <a.kupka1@gmx.net>
2024-08-07 21:27:27 +0200

    making only bookmark icon of first card change after clicking - according to requirements

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ console.clear();
 
 console.log("JS is working");
 
-const button = document.querySelector('[data-js="bookmark-button"]');
-const bookmarkImage = document.querySelector('[data-js="bookmark-img"]');
+const button = document.querySelector('[data-js="bookmark-button-1"]');
+const bookmarkImage = document.querySelector('[data-js="bookmark-img-1"]');
 
 button.addEventListener("click", () => {
   if (bookmarkImage.src.includes("bookmark-black.svg")) {
