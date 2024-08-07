e92f501b501843320fd78b3956a2aadc78cc0d64
andreask <a.kupka1@gmx.net>
2024-08-07 22:31:38 +0200

    answer now shown or hidden when clicking button - button text and style also change

--- js/index.js
+++ js/index.js
@@ -1,14 +1,28 @@
 console.clear();
 
-console.log("JS is working");
-
-const button = document.querySelector('[data-js="bookmark-button-1"]');
+const bookmarkButton = document.querySelector('[data-js="bookmark-button-1"]');
 const bookmarkImage = document.querySelector('[data-js="bookmark-img-1"]');
+const answerButton = document.querySelector('[data-js="answer-button-1"]');
+const answerText = document.querySelector('[data-js="answer-1"]');
 
-button.addEventListener("click", () => {
+bookmarkButton.addEventListener("click", () => {
   if (bookmarkImage.src.includes("bookmark-black.svg")) {
     bookmarkImage.src = "./img/bookmark-check-black.svg";
   } else {
     bookmarkImage.src = "./img/bookmark-black.svg";
+  }
+});
+
+answerButton.addEventListener("click", () => {
+  if (answerButton.textContent.trim() === "Show Answer") {
+    answerButton.classList.remove("button--show-answer");
+    answerButton.classList.add("button--hide-answer");
+    answerButton.textContent = "Hide Answer";
+    answerText.style.display = "flex";
+  } else {
+    answerButton.classList.remove("button--hide-answer");
+    answerButton.classList.add("button--show-answer");
+    answerButton.textContent = "Show Answer";
+    answerText.style.display = "none";
   }
 });
